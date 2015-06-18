cb86ade6c9b0ac703a4ae8c99b71dfaf73bee792
Mikhail Fokin <fokin.m@vpb.su>
2015-06-18 17:10:57 +0300

    Initial Commit

---
+++ AbbyyTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AbbyyTest
+//
+//  Created by Mikhail Fokin on 18.06.15.
+//  Copyright (c) 2015 Mikhail Fokin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
